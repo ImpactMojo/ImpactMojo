a9161e25d55875ce6e1fa266df487b8fad052726
Varna Sri Raman <107885988+Varnasr@users.noreply.github.com>
2025-07-26 22:29:30 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -299,7 +299,7 @@ function App() {
 
 // Navbar Component
 function Navbar() {
-  const { user, currentPage, setCurrentPage, handleSignOut } = useAuth();
+  const { user, currentPage, setCurrentPage, handleSignOut, signInWithGoogle } = useAuth();
   const { darkMode, toggleTheme } = useTheme();
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
 
@@ -786,7 +786,7 @@ function LabCard({ lab }) {
           {lab.description}
         </p>
         
-        <a
+        
           href={lab.content}
           target="_blank"
           rel="noopener noreferrer"
@@ -816,7 +816,7 @@ function ResourceCard({ resource }) {
         {resource.description}
       </p>
       
-      <a
+      
         href={resource.link}
         target="_blank"
         rel="noopener noreferrer"
