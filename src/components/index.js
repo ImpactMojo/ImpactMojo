3c400b5710dd0edfae24dd45471dfbd89b31ace0
Varna Sri Raman <107885988+Varnasr@users.noreply.github.com>
2025-09-08 19:19:18 +0530

    Update index.js

--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,14 @@
+// This file acts as a "map" for all components in this folder.
+// It makes imports cleaner in other parts of the app.
+
 export { default as Logo } from './Logo.js';
 export { default as Navigation } from './Navigation.js';
 export { default as QuizResult } from './QuizResult.js';
 export { default as SimpleQuiz } from './SimpleQuiz.js';
-export { ImprovedFloatingActionButtons } from './floating-action-buttons.js';
-export { FeaturedContentSection, TestimonialsSection, PopularCoursesSection } from './homepage-components.js';
-export { LearningTracksSection } from './learning-tracks-component.js';
+export { default as Footer } from './Footer.js'; // Added the new Footer component
+
+// These files export multiple components, so we use `export *`
+export * from './floating-action-buttons.js';
+export * from './homepage-components.js';
+export * from './learning-tracks-component.js';
+
