b22264505a121b68c39c8df609fcaa34ea1c798d
Varna Sri Raman <107885988+Varnasr@users.noreply.github.com>
2025-09-08 19:06:19 +0530

    Update index.js

--- src/data/index.js
+++ src/data/index.js
@@ -4,3 +4,4 @@ export * from './featured-content-data.js';
 export * from './labs-data.js';
 export * from './premium-resources-data.js';
 export * from './testimonials-data.js';
+export * from './track-styling-data.js';
