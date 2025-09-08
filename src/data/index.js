6deaca5c62181affb125fcb08183fc720f3f6011
Varna Sri Raman <107885988+Varnasr@users.noreply.github.com>
2025-09-08 18:59:16 +0530

    Create index.js

---
+++ src/data/index.js
@@ -0,0 +1,6 @@
+export * from './ai-tools-data.js';
+export * from './course-data.js';
+export * from './featured-content-data.js';
+export * from './labs-data.js';
+export * from './premium-resources-data.js';
+export * from './testimonials-data.js';
